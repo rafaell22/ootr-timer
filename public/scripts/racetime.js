d0888634989aa6b8e8f61ce679860da143d63bfa
rafaellfpc <rafaell@fleetprofitcenter.com>
2023-10-16 21:34:29 -0500

    Finished integration with racetime.gg. After race selected, the timer will automatically start a few seconds after the "real" start (from the countdown).

--- public/scripts/racetime.js
+++ public/scripts/racetime.js
@@ -1,7 +1,9 @@
 // @ts-check
 import { show, hide } from './domUtils.js';
 import Race from './classes/Race.js';
+import { setStartTime } from './settings.js';
 import tinyduration from './tinyduration.js';
+import { startTimer } from './timer.js';
 
 const dialogOverlay = document.getElementById('dialog-overlay');
 const racetime = {
@@ -18,107 +20,121 @@ dialogOverlay?.addEventListener('click', function closeRacetimeDialog() {
     hide(dialogOverlay);
 });
 
-const raceInformation = {};
-/**
- * @param {boolean} isBareView
- */
-export function setViewMode(isBareView) {
-    if(!isBareView) {
-        raceInformation.raceName = document.getElementById('race-name');
-        raceInformation.raceCategory = document.getElementById('race-category');
-        hide(racetimeButton);
-        return;
-    }
-
-    window.resizeTo(450, 450);
-}
+const raceInformation = {
+    raceName: document.getElementById('race-name'),
+    raceCategory: document.getElementById('race-category'),
+};
 
 let races;
 let categories;
-racetimeButton?.addEventListener('click', async function openRacetimeDialog() {
-    show(loader);
-    try {
-        categories = await Race.loadCategories();
-        categories.sort((a, b) => {
-            return a.name.localeCompare(b.name);
-        })
 
-        if(racetime.selectCategory) {
-            racetime.selectCategory.innerHTML = `
-                <option disabled selected>Select category...</option>
-            `;
+export function initRacetimeEvents() {
+    racetimeButton?.addEventListener('click', async function openRacetimeDialog() {
+        show(loader);
+        try {
+            categories = await Race.loadCategories();
+            categories.sort((a, b) => {
+                return a.name.localeCompare(b.name);
+            })
+
+            if(racetime.selectCategory) {
+                racetime.selectCategory.innerHTML = `
+                    <option disabled selected>Select category...</option>
+                `;
+            }
+
+            for(const category of categories) {
+                const option = document.createElement('option');
+                option.value = category.slug;
+                option.textContent = category.name;
+                racetime.selectCategory?.appendChild(option);
+            }
+
+            show(dialogOverlay);
+            show(racetime.dialog);
+            show(racetime.selectCategory);
+        } catch(errorGettingRacesList) {
+            console.error('ERROR');
+            console.error(errorGettingRacesList.stack);
+        } finally {
+            hide(loader);
         }
+    });
 
-        for(const category of categories) {
-            const option = document.createElement('option');
-            option.value = category.slug;
-            option.textContent = category.name;
-            racetime.selectCategory?.appendChild(option);
+    let selectedCategory;
+    racetime.selectCategory?.addEventListener('change', async function loadRaces(event) {
+        show(loader);
+        const categorySlug = event.target?.['value'];
+        selectedCategory = categories.find(category => category.slug === categorySlug);
+        try {
+            races = await Race.loadRacesByCategory(categorySlug);
+            if(racetime.selectRace) {
+                racetime.selectRace.innerHTML = `
+                    <option disabled selected>Select race...</option>
+                `;
+            }
+            for(const race of races) {
+                const option = document.createElement('option');
+                option.value = race.name;
+                option.textContent = race.info;
+                racetime.selectRace?.appendChild(option);
+            }
+            show(racetime.selectRace);
+            show(racetime.selectButton);
+        } catch(errorGettingRacesList) {
+            console.error('ERROR');
+            console.error(errorGettingRacesList);
+        } finally {
+            hide(loader);
         }
+    });
 
-        show(dialogOverlay);
-        show(racetime.dialog);
-        show(racetime.selectCategory);
-    } catch(errorGettingRacesList) {
-        console.error('ERROR');
-        console.error(errorGettingRacesList.stack);
-    } finally {
-        hide(loader);
+    let selectedRace;
+    racetime.selectButton?.addEventListener('click', async function selectRace() {
+        show(loader);
+        hide(dialogOverlay);
+        hide(racetime.dialog);
+        const raceSlug = racetime.selectRace?.['value'];
+        selectedRace = races.find(race => race.name === raceSlug);
+        try {
+            await checkForRaceStart();
+            if(raceInformation?.raceCategory) {
+                raceInformation.raceCategory.textContent = selectedCategory.name;
+            }
+            if(raceInformation?.raceName) {
+                raceInformation.raceName.textContent = selectedRace.info;
+            }
+
+        } catch(errorGettingRacesDetails) {
+            console.error('ERROR!');
+            console.error(errorGettingRacesDetails);
+        } finally {
+            hide(loader);
+        }
+    });
+
+    async function checkForRaceStart() {
+        try {
+            const raceSlug = selectedRace.name;
+            const raceDetailsResult = await Race.loadRaceDetails(raceSlug);
+            const startDelay = tinyduration.parse(raceDetailsResult.start_delay);
+            const startDelayInSeconds = startDelay.seconds + startDelay.minutes * 60  + startDelay.hours * 60 * 60;
+            let raceTime = 0;
+            if(!raceDetailsResult.started_at) {
+                raceTime = -startDelayInSeconds * 1000;
+                setStartTime(raceTime);
+                setTimeout(checkForRaceStart, startDelayInSeconds * 1000 / 3);
+            } else {
+                const startTime = new Date(raceDetailsResult.started_at).getTime();
+                const currentTime = new Date(raceDetailsResult.date_exact).getTime();
+                raceTime = currentTime - startTime;
+                setStartTime(raceTime);
+                startTimer();
+            }
+        } catch(errorGettingRacesDetails) {
+            console.error('ERROR!');
+            console.error(errorGettingRacesDetails);
+        }
     }
-});
-
-let selectedCategory;
-racetime.selectCategory?.addEventListener('change', async function loadRaces(event) {
-    show(loader);
-    const categorySlug = event.target?.['value'];
-    selectedCategory = categories.find(category => category.slug === categorySlug);
-    console.log('selectCategory: ', selectedCategory);
-    try {
-        races = await Race.loadRacesByCategory(categorySlug);
-        console.log('races: ', races);
-        if(racetime.selectRace) {
-            racetime.selectRace.innerHTML = `
-                <option disabled selected>Select race...</option>
-            `;
-        }
-        for(const race of races) {
-            const option = document.createElement('option');
-            option.value = race.name;
-            option.textContent = race.info;
-            racetime.selectRace?.appendChild(option);
-        }
-        show(racetime.selectRace);
-        show(racetime.selectButton);
-    } catch(errorGettingRacesList) {
-        console.error('ERROR');
-        console.error(errorGettingRacesList);
-    } finally {
-        hide(loader);
-    }
-});
-
-let selectedRace;
-racetime.selectButton?.addEventListener('click', async function selectRace() {
-    show(loader);
-    hide(dialogOverlay);
-    hide(racetime.dialog);
-    const raceSlug = racetime.selectRace?.['value'];
-    selectedRace = races.find(race => race.name === raceSlug);
-    try {
-        const raceDetailsResult = await Race.loadRaceDetails(raceSlug);
-        console.log('raceDetailsResult: ', raceDetailsResult);
-        if(raceInformation?.raceCategory) {
-            raceInformation.raceCategory.textContent = selectedCategory.name;
-        }
-
-        if(raceInformation?.raceName) {
-            raceInformation.raceName.textContent = selectedRace.info;
-        }
-    } catch(errorGettingRacesDetails) {
-        console.error('ERROR!');
-        console.error(errorGettingRacesDetails);
-    } finally {
-        hide(loader);
-    }
-});
+}
 
